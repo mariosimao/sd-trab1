b9b3ba22ba2810eea4afd2b32bf76221f244ce64
Mario Simão <mariosimao@poli.ufrj.br>
2022-05-05 22:06:00 -0300

    Remove unused includes

--- socket.cpp
+++ socket.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <sys/socket.h>
-// #include <stdio.h>
 #include <stdlib.h>
-// #include <string.h>
 #include <unistd.h>
-// #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include "common.h"
